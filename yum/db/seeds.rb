0578c0595272186f09187d5a706551968abb7367
DeCode2018 <decode2018@gmail.com>
2018-11-12 10:10:56 -0500

    added backend folder

---
+++ yum/db/seeds.rb
@@ -0,0 +1,24 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+#---------------------Recipes Seed Data-----------------------------------------
+Recipe.create(name: 'Shrimp Alfredo', image_url: 'https://assets.kraftfood
+s.com/recipe_images/opendeploy/113758_640x428.jpg', ingredients:'shrimp, linguine, alfr
+edo sauce',cook_time: '20 mins', instructions: 'instructions go here')
+
+Recipe.create(name: 'Macaroni & Cheese', image_url: 'https://assets.kraftfood
+s.com/recipe_images/opendeploy/113758_640x428.jpg', ingredients:'elbow macaroni noodles, chedder cheese',cook_time: '25 mins', instructions: 'instructions go here')
+
+Recipe.create(name: 'Steak with Horseradish Sauce', image_url: 'https://assets.kraftfood
+s.com/recipe_images/opendeploy/113758_640x428.jpg', ingredients:'rib-eye steak, Horseradish sauce',cook_time: '18 mins', instructions: 'instructions go here')
+
+Recipe.create(name: 'Panang Shrimp', image_url: 'https://assets.kraftfood
+s.com/recipe_images/opendeploy/113758_640x428.jpg', ingredients:'shrimp, panang sauce',cook_time: '15 mins', instructions: 'instructions go here')
+
+Recipe.create(name: 'Sesame Chicken', image_url: 'https://assets.kraftfood
+s.com/recipe_images/opendeploy/113758_640x428.jpg', ingredients:'Chicken, seasame sauce',cook_time: '20 mins', instructions: 'instructions go here')
